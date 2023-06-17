686f9a343b8f96745722c9fcc128168e1d9dd285
AirZED <regnalukpabio@gmail.com>
2023-06-17 01:55:01 +0100

    fetched data that would be pushed to meals db

---
+++ src/extras/addMealstoDb.js
@@ -0,0 +1,40 @@
+const dbConnect = require('./../db');
+const fs = require('fs');
+const Meal = require('../models/mealModel');
+
+const { fetch } = require('popsicle');
+
+const fetchMeals = async () => {
+  try {
+    // await dbConnect();
+    const response = await fetch(
+      'https://react-shopping-app-9b16c-default-rtdb.firebaseio.com/DUMMY_MEALS.json'
+    );
+
+    if (!response.ok) {
+      throw new Error('THis was an error fetching meals');
+    }
+
+    const data = await response.text();
+
+    const stringifiedData = JSON.parse(data);
+
+    const createdMeals = await Meal.create(stringifiedData);
+
+    fs.writeFile(`./meals.json`, JSON.stringify(stringifiedData), (err) => {
+      if (err) {
+        console.log('The write to file did not work');
+        process.exit();
+      }
+
+      console.log('Successful');
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+};
+
+if (process.argv[2] === '--fetch') {
+  fetchMeals();
+}
